8d0c18e183c41291209cc39439fd145bb0e00b2e
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2018-10-24 23:29:21 +0300

    inital commit

---
+++ src/Deck.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends Component{
+
+    renderCards(){
+        return this.props.data.map( item => {
+            return this.props.renderCard(item)
+        })
+    }
+
+    render(){
+        return (
+        <View>
+            {this.renderCards()}
+        </View>
+        );
+    }
+}
+
+
+export default Deck;
